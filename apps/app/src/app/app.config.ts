22e9505c7148813bbf067a20df0114868bd9706f
Nicholas Krasuski <nkr@nemlig.com>
2024-09-25 17:32:55 +0200

    adjust output path

--- apps/app/src/app/app.config.ts
+++ apps/app/src/app/app.config.ts
@@ -1,13 +1,10 @@
-import {
-  ApplicationConfig,
-  provideExperimentalZonelessChangeDetection,
-} from '@angular/core';
+import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
 import { provideRouter } from '@angular/router';
 import { appRoutes } from './app.routes';
 
 export const appConfig: ApplicationConfig = {
   providers: [
-    provideExperimentalZonelessChangeDetection(),
+    provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(appRoutes),
   ],
 };
